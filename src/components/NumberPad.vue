ff6a178ee5c64a08991f8b48a1870a8e17db9694
Dennis Sauve <dengsauve@yahoo.com>
2020-07-04 15:21:06 -0700

    Refactored numberpad layout
    Initially I was still playing around with the layouts, wanted to get the look I wanted before going data driven.

--- src/components/NumberPad.vue
+++ src/components/NumberPad.vue
@@ -1,57 +1,13 @@
 <template>
     <div class="number-pad">
 
-        <div class="row">
-            <number-button title="one"/>
-            <number-button title="two"/>
-            <number-button title="three"/>
+        <div class="row" v-for="(row, idx) in buttons.rows" :key="idx">
+            <number-button v-for="button in row"
+                           :title="button"
+                           :key="button"
+                           v-on:number-stack="numberPressed($event)"
+            />
         </div>
-        <div class="row">
-            <number-button title="four"/>
-            <number-button title="five"/>
-            <number-button title="six"/>
-        </div>
-        <div class="row">
-            <number-button title="seven"/>
-            <number-button title="eight"/>
-            <number-button title="nine"/>
-        </div>
-        <div class="row">
-            <number-button title="ten"/>
-            <number-button title="eleven"/>
-            <number-button title="twelve"/>
-        </div>
-        <div class="row">
-            <number-button title="thirteen"/>
-            <number-button title="fourteen"/>
-            <number-button title="fifteen"/>
-        </div>
-        <div class="row">
-            <number-button title="sixteen"/>
-            <number-button title="seventeen"/>
-            <number-button title="eighteen"/>
-        </div>
-        <div class="row">
-            <number-button title="nineteen"/>
-            <number-button title="twenty"/>
-            <number-button title="thirty"/>
-        </div>
-        <div class="row">
-            <number-button title="forty"/>
-            <number-button title="fifty"/>
-            <number-button title="sixty"/>
-        </div>
-        <div class="row">
-            <number-button title="seventy"/>
-            <number-button title="eighty"/>
-            <number-button title="ninety"/>
-        </div>
-        <div class="row">
-            <number-button title="hundred"/>
-            <number-button title="thousand"/>
-            <number-button title="million"/>
-        </div>
-
 
     </div>
 </template>
@@ -64,7 +20,32 @@
             'number-button': NumberButton
         },
 
-        name: "number-pad"
+        name: "number-pad",
+
+        data: function () {
+            return {
+                buttons: {
+                    rows: [
+                        ["one", "two", "three"],
+                        ["four", "five", "six"],
+                        ["seven", "eight", "nine"],
+                        ["ten", "eleven", "twelve"],
+                        ["thirteen", "fourteen", "fifteen"],
+                        ["sixteen", "seventeen", "eighteen"],
+                        ["nineteen", "twenty", "thirty"],
+                        ["forty", "fifty", "sixty"],
+                        ["seventy", "eighty", "ninety"],
+                        ["hundred", "thousand", "million"],
+                    ]
+                }
+            }
+        },
+
+        methods: {
+            numberPressed: function ($event) {
+                this.$emit('number-stack', $event);
+            }
+        }
     }
 </script>
 
