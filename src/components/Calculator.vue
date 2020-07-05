7bc2b00581d432813916d86e46e7b465728cfa6d
Dennis Sauve <dengsauve@yahoo.com>
2020-07-04 21:06:38 -0700

    Update Calculator.vue

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -8,44 +8,81 @@
 </template>
 
 <script>
-import Display from "./Display";
-import Keypad from "./Keypad";
+    import Display from "./Display";
+    import Keypad from "./Keypad";
 
-export default {
-    components: {
-        Display,
-        Keypad
-    },
-    data: function () {
-        return {
-            message: 'Welcome'
-        }
-    },
-
-    methods: {
-        newNumber: function ($event) {
-            if (this.message === 'Welcome') {
-                this.message = '';
-            }
-            this.message += $event + ' ';
+    export default {
+        components: {
+            Display,
+            Keypad
         },
-        newOperator: function ($event) {
-            // Clear
-            if ($event === 'C') {
-                this.message = '';
+        data: function () {
+            return {
+                message: 'Welcome',
+                dictionary: {
+                    'one': 1,
+                    'two': 2,
+                    'three': 3,
+                    "four": 4,
+                    "five": 5,
+                    "six": 6,
+                    "seven": 7,
+                    "eight": 8,
+                    "nine": 9,
+                    "ten": 10,
+                    "eleven": 11,
+                    "twelve": 12,
+                    "thirteen": 13,
+                    "fourteen": 14,
+                    "fifteen": 15,
+                    "sixteen": 16,
+                    "seventeen": 17,
+                    "eighteen": 18,
+                    "nineteen": 19,
+                    "twenty": 20,
+                    "thirty": 30,
+                    "forty": 40,
+                    "fifty": 50,
+                    "sixty": 60,
+                    "seventy": 70,
+                    "eighty": 80,
+                    "ninety": 90,
+                    "hundred": 100,
+                    "thousand": 1000,
+                    "million": 1000000,
+                }
             }
-            // Equate
-            else if ($event === '=') {
-                // Handle equation
-                this.message += '\n' + $event + '\n';
-            }
-            // Add symbol to message stack
-            else {
-                this.message += '\n' + $event + '\n';
+        },
+
+        methods: {
+            newNumber: function ($event) {
+                if (this.message === 'Welcome') {
+                    this.message = '';
+                }
+                this.message += $event + ' ';
+            },
+            newOperator: function ($event) {
+                // Clear
+                if ($event === 'C') {
+                    this.message = '';
+                }
+                // Equate
+                else if ($event === '=') {
+                    // Handle equation
+                    this.parseMessage();
+                    this.message += '\n' + $event + '\n';
+                }
+                // Add symbol to message stack
+                else {
+                    this.message += '\n' + $event + '\n';
+                }
+            },
+            parseMessage: function () {
+                let lineArray = this.message.split('\n');
+                console.log(lineArray);
             }
         }
     }
-}
 </script>
 
 <style scoped>
