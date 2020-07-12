8952b8e8b1cf2954a36fea1f383b8440dc7189a0
Dennis Sauve <dengsauve@yahoo.com>
2020-07-12 16:55:49 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,13 @@ npm run build
 cd dist
 
 # Commit it
+git init
+git remote add origin https://github.com/dengsauve/Text-Calculator.git
+git checkout -b gh-pages
 git add -A
 git commit -m 'deployment'
 
 # Ship it
-git push -u origin gh-pages
+git push -f origin gh-pages
 
 cd -
